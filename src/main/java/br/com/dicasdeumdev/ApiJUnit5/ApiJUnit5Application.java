3516a1200fce0bf427fd290d4ff98a072cc5b924
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2023-01-13 20:38:55 -0300

    Projeto criado

---
+++ src/main/java/br/com/dicasdeumdev/ApiJUnit5/ApiJUnit5Application.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.ApiJUnit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJUnit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJUnit5Application.class, args);
+	}
+
+}
