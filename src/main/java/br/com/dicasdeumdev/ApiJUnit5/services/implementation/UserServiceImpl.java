26b61c17780293dad5308a58ecd52b8a4cb15da2
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2023-01-13 21:30:49 -0300

    Classe UserServiceImpl findById

---
+++ src/main/java/br/com/dicasdeumdev/ApiJUnit5/services/implementation/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.dicasdeumdev.ApiJUnit5.services.implementation;
+
+import br.com.dicasdeumdev.ApiJUnit5.domain.User;
+import br.com.dicasdeumdev.ApiJUnit5.repositories.UserRepository;
+import br.com.dicasdeumdev.ApiJUnit5.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
