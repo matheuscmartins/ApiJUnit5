94fb5e70a10e8c4d8bafb1d8abe30d24e72ae0c0
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2023-01-13 21:31:12 -0300

    Classe UserResource findById

--- src/main/java/br/com/dicasdeumdev/ApiJUnit5/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/ApiJUnit5/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.dicasdeumdev.ApiJUnit5.resources;
 
 import br.com.dicasdeumdev.ApiJUnit5.domain.User;
+import br.com.dicasdeumdev.ApiJUnit5.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> fingById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Matheus", "matheus@gmail.com","123"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
