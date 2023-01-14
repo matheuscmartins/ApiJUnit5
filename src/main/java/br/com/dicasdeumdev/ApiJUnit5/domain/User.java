76f965b3fc912b7dadccec1111affeffabceea8e
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2023-01-13 21:05:38 -0300

    Classe User

---
+++ src/main/java/br/com/dicasdeumdev/ApiJUnit5/domain/User.java
@@ -0,0 +1,20 @@
+package br.com.dicasdeumdev.ApiJUnit5.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //substitui hashCodeEquals, ToString e Getters Setters, porem se a classe tiver muitos atributos abaix a performace da API
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
