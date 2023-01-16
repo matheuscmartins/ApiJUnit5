2abd1bcd2bfa7830382786016bbc7e46178c96bb
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2023-01-15 22:04:51 -0300

    Teste para manipulador de Exception metodo DataIntegrity

--- src/test/java/br/com/dicasdeumdev/ApiJUnit5/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/dicasdeumdev/ApiJUnit5/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.ApiJUnit5.resources.exceptions;
 
+import br.com.dicasdeumdev.ApiJUnit5.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.ApiJUnit5.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.time.LocalDateTime;
+
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
@@ -38,9 +42,22 @@ class ResourceExceptionHandlerTest {
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals(OBJETO_NAO_ENCONTRADO_NO_ID, response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler.dataIntegrityViolationException(
+                new DataIntegratyViolationException("E-mail ja cadastrado"),
+                new MockHttpServletRequest());
+
+        assertNotNull(response); //assegura que não será null
+        assertNotNull(response.getBody()); //assegura que o body não é null
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("E-mail ja cadastrado", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
